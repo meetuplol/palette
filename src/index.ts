8dc737c19076f194cc041c60b419214e406b6523
Tristan Camejo <contact@tristancamejo.com>
2022-10-03 01:20:25 +1100

    scss var types and refactor

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
-export enum Palette {
+/**
+ * MeetUp color palette hex codes.
+ */
+export enum PaletteHex {
 	dark = '#262626',
 	light = '#f9f5e3',
 	green = '#499167',
 	pink = '#ef9cda',
 	flame = '#d36135'
 }
+
+/**
+ * MeetUp valid SCSS color variables.
+ */
+export type Palette = keyof typeof PaletteHex;
